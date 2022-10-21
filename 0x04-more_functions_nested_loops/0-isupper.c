c6a81f02e54cf71b1a57fd15c9f33259e6adb520
Ebikedoubo <greatestebi@gmail.com>
2022-10-21 08:58:11 -0700

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,18 +1,17 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
-* main - check the code.
-*
-* Return: Always 0.
-*/
-int main(void)
-{
-	    char c;
+ *  * _isdigit - checks for characters that are digits
+ *   * @c: character to check
+ *    *
+ *     * Return: 0 or 1
+ *      */
 
-	        c = 'A';
-		    printf("%c: %d\n", c, _isupper(c));
-		        c = 'a';
-			    printf("%c: %d\n", c, _isupper(c));
-			        return (0);
+
+int _isdigit(int c)
+{
+		if (c >= '0' && c <= '9')
+					return (1);
+			else
+						return (0);
 }
